fae141ac802326d103b24f86f095a62209725246
Wtlm <chunganh1312@gmail.com>
2023-11-30 00:02:27 +0700

    check size frame

--- src/Interface/Panel.java
+++ src/Interface/Panel.java
@@ -1,24 +1,25 @@
 package Interface;
+
 import java.awt.*;
 import javax.swing.*;
 
 import Stages.Intro;
 import Stages.Option;
 
-
-public class Panel extends JPanel{
+public class Panel extends JPanel {
 
     public Intro intro;
     public Option option;
+
     public Panel() {
         this.setFocusable(true);
-        // intro = new Intro(this);
-        option = new Option(this);
+        intro = new Intro(this);
+        // option = new Option(this);
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // intro.draw(g);
+        intro.draw(g);
     }
-    
+
 }
--- src/Stages/Intro.java
+++ src/Stages/Intro.java
@@ -1,4 +1,5 @@
 package Stages;
+
 import java.awt.*;
 
 import javax.swing.*;
@@ -13,10 +14,11 @@ public class Intro {
     public Image bgr;
     MouseHandler mouseHandler;
     Panel panel;
+
     public Intro(Panel panel) {
         this.panel = panel;
         try {
-            bgr = ImageIO.read(new File("D:/DSA/chart/Chart/Data/Cover.png")); 
+            bgr = ImageIO.read(new File("D:/DSA/chart/Data/Cover.png"));
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -24,21 +26,21 @@ public class Intro {
     }
 
     public void draw(Graphics g) {
-        g.drawImage(bgr, 0, 0, null);
+        g.drawImage(bgr, 0, 0, 1200, 600, null);
         DisplayIntro(panel);
     }
 
     public void DisplayIntro(Panel panel) {
-        ImageIcon icon = new ImageIcon("D:/DSA/chart/Chart/Data/Cover.gif");
+        ImageIcon icon = new ImageIcon("D:/DSA/chart/Data/Cover.gif");
         JButton button = new JButton("Let Start!");
         button.setFont(new Font("Quicksand", Font.PLAIN, 16));
         button.setForeground(Color.white);
-        button.setBounds(600,380,100,40); 
+        button.setBounds(280, 350, 100, 40);
         button.setBackground(Color.decode("#7806A6"));
         button.addMouseListener(mouseHandler);
         panel.add(button);
         JButton button1 = new JButton(icon);
-        button1.setBounds(900,150,490,490); 
+        button1.setBounds(650, 50, 490, 490);
         panel.add(button1);
     }
 }
