e42bcd5513aecb7b7e65fe693d48e7ffc5d0fa6e
Jiang <95130977+ititiu21193@users.noreply.github.com>
2023-12-15 17:29:09 +0700

    edit option

--- src/Action/MouseHandler.java
+++ src/Action/MouseHandler.java
@@ -2,10 +2,11 @@ package Action;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import Interface.Panel;
 
-public class MouseHandler implements MouseListener {
+public class MouseHandler implements MouseListener{
     Panel panel;
 
     public MouseHandler(Panel panel) {
@@ -15,11 +16,20 @@ public class MouseHandler implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-
+        if (panel.option.isIn(e, panel.option.pie1))
+            panel.option.choosePie = 1;
+        else panel.option.choosePie = 0;
+        
+        if (panel.option.isIn(e, panel.option.bar1)){
+            panel.option.chooseBar = 1;
+            System.out.println("hahaha");
+        }
+        else panel.option.chooseBar = 0;
     }
 
     @Override
@@ -30,12 +40,15 @@ public class MouseHandler implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
+        
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
+        if (!panel.option.isIn(e, panel.option.pie1))
+            panel.option.choosePie = 0;
+        else if (!panel.option.isIn(e, panel.option.bar1))
+            panel.option.chooseBar = 0;
 
     }
 
--- src/Stages/Option.java
+++ src/Stages/Option.java
@@ -1,37 +1,40 @@
 package Stages;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import javax.swing.border.*;
 
+import Action.MouseHandler;
 import Interface.Panel;
 
 public class Option {
     Panel panel;
-    BufferedImage bgr;
+    public BufferedImage bgr, pie1, pie2, bar1, bar2;
     JLabel label;
-    ImageIcon pie, bar;
-    JButton pieButton, barButton;
     private int height;
+    public int chooseBar, choosePie;
 
     public Option(Panel panel) {
         this.panel = panel;
         label = new JLabel();
-        pieButton = new JButton();
-        barButton = new JButton();
+        chooseBar = 0;
+        choosePie = 0;
         height = 600;
         try {
 
             bgr = ImageIO.read(new File("D:/DSA/chart/Data/BGR.png"));
-            pie = new ImageIcon("D:/DSA/chart/Data/Pie.png");
-            bar = new ImageIcon("D:/DSA/chart/Data/Bar.png");
+            pie1 =  ImageIO.read(new File("D:/DSA/chart/Data/pie1.png"));
+            pie2 =  ImageIO.read(new File("D:/DSA/chart/Data/pie2.png"));
+            bar1 =  ImageIO.read(new File("D:/DSA/chart/Data/bar1.png"));
+            bar2 =  ImageIO.read(new File("D:/DSA/chart/Data/bar2.png"));
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -41,12 +44,17 @@ public class Option {
     }
 
     public void draw(Graphics2D g2) {
-        // long a = System.currentTimeMillis();
-        // int b = 600;
-        // while (b >= 0) {
         g2.drawImage(bgr, 0, getHeight(), 1200, 600, null);
-        // panel.repaint();
-        // }
+        if (chooseBar == 0) 
+            g2.drawImage(bar1, 700, getHeight() + 100, 400, 400, null);
+        else 
+            g2.drawImage(bar2, 700, getHeight() + 100, 400, 400, null);
+
+        if (choosePie == 0) 
+            g2.drawImage(pie1, 100, getHeight() + 100, 400, 400, null);
+        else 
+            g2.drawImage(pie2, 100, getHeight() + 100, 400, 400, null);
+
         addButton(panel);
     }
 
@@ -59,17 +67,19 @@ public class Option {
         label.setText("Choose a chart");
         label.setBounds(0, getHeight(), panel.getWidth(), 100);
         label.setHorizontalAlignment(JLabel.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 30));
-        label.setForeground(Color.white);
+        label.setFont(new Font("Arial", Font.BOLD, 40));
+        label.setForeground(Color.WHITE);
         panel.add(label);
 
-        pieButton.setIcon(pie);
-        pieButton.setBounds(100, getHeight() + 100, 400, 400);
-        panel.add(pieButton);
+        // pieButton.setBounds(100, getHeight() + 100, 400, 400);
+        // pieButton.setIcon(pie);
+        // pieButton.setBorder(new RoundedBorder(200));
+        // pieButton.setBackground(Color.WHITE);
+        // panel.add(pieButton);
 
-        barButton.setIcon(bar);
-        barButton.setBounds(700, getHeight() + 100, 400, 400);
-        panel.add(barButton);
+        // barButton.setIcon(bar);
+        // barButton.setBounds(700, getHeight() + 100, 400, 400);
+        // panel.add(barButton);
     }
 
     public int getHeight() {
@@ -80,4 +90,12 @@ public class Option {
         this.height = height;
     }
 
+    public boolean isIn(MouseEvent e, BufferedImage image) {
+        if (e.getX() >= image.getMinX() && e.getX() <= (image.getMinX()+ image.getWidth()) 
+            && e.getY() >= image.getMinY() && e.getY() <= (image.getMinY()+ image.getHeight())){
+            return true;
+        } 
+        else return false;
+    }
 }
+
